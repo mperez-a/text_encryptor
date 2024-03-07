9e2080189ea8d6cd3c5f386f42a7f6f3c2b8bc94
Manuela Perez <manuelaperezag@gmail.com>
2024-03-07 20:24:20 +0100

    encript and decript finished

--- index.js
+++ index.js
@@ -11,9 +11,25 @@ function btnEncrypt() {
 		btnCopy.innerHTML = 'Copiar';
 		btnCopy.className = 'btn-copy';
 		document.querySelector('.copy').appendChild(btnCopy);
+		btnCopy.addEventListener('click', copy);
+	} else {
+		document.querySelector('.message').style.backgroundImage = "url('/img/doll.png')";
+		document.querySelector('.btn-copy').remove();
 	}
+}
 
-	if (messageShowed.value === '') {
+function btnDecrypt() {
+	const decriptedText = decrypt(message.value);
+	messageShowed.value = decriptedText;
+
+	if (messageShowed.value !== '') {
+		document.querySelector('.message').style.backgroundImage = "none";
+		let btnCopy = document.createElement('button');
+		btnCopy.innerHTML = 'Copiar';
+		btnCopy.className = 'btn-copy';
+		document.querySelector('.copy').appendChild(btnCopy);
+		btnCopy.addEventListener('click', copy);
+	} else {
 		document.querySelector('.message').style.backgroundImage = "url('/img/doll.png')";
 		document.querySelector('.btn-copy').remove();
 	}
@@ -28,4 +44,25 @@ function encrypt(encriptedMessage) {
 		}
 	}
 	return encriptedMessage;
+}
+
+function decrypt(encriptedMessage) {
+	let keys = [["enter", "e"], ["imes", "i"], ["ai", "a"], ["ober", "o"], ["ufat", "u"]];
+	encriptedMessage = encriptedMessage.toLowerCase();
+	for (let i = 0; i < keys.length; i++) {
+		if (encriptedMessage.includes(keys[i][0])) {
+			encriptedMessage = encriptedMessage.replaceAll(keys[i][0], keys[i][1]);
+		}
+	}
+	return encriptedMessage;
+}
+
+function copy() {
+	navigator.clipboard.writeText(messageShowed.value)
+	.then(() => {
+		alert('Texto copiado');
+	})
+	.catch(err => {
+		alert('Error al copiar el texto');
+	});
 }
